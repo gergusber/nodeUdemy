f5f0de452a1a3c81d3fd35f1d1376e4b21fe2f38
gerbertea <gerbertea@gmail.com>
2020-04-10 14:54:51 -0300

    37

--- NodeJSCompleteGuide/app.js
+++ NodeJSCompleteGuide/app.js
@@ -23,12 +23,12 @@ const server = http.createServer((req, res) => {
            const parseBody = Buffer.concat(body).toString();
           console.log(parseBody);
            const message = parseBody.split('=')[1];
-           fs.writeFileSync('message.txt',message);
-
+           fs.writeFile('message.txt',message,(err)=>{
+            res.statusCode= 302;
+            res.setHeader('Location','/')
+            return res.end();
+           });
         });
-        res.statusCode= 302;
-        res.setHeader('Location','/')
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
