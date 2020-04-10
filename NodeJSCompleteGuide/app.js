367510312dcc5567af16d73a4df51330a32f17a1
gerbertea <gerbertea@gmail.com>
2020-04-10 14:14:33 -0300

    LESSON 35

--- NodeJSCompleteGuide/app.js
+++ NodeJSCompleteGuide/app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
 
     if (url === '/') {
         res.write('<html>');
@@ -10,7 +12,24 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
-
+    if(url === '/message' && method ==='POST'){
+        const body=[];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+            
+        });
+        req.on('end',()=>{
+           const parseBody = Buffer.concat(body).toString();
+           console.log(parseBody);
+           const message = parseBody.split('=')[1];
+           fs.writeFileSync('message.txt',message);
+        });
+        
+        res.statusCode= 302;
+        res.setHeader('Location','/')
+        return res.end();
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
