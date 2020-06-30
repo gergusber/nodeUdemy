88eb856719ad12295ef405b6db8a7bd1b1d02c8c
gerbertea <gerbertea@gmail.com>
2020-06-30 21:41:32 +0200

    undestand middleware concept

--- NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
+++ NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app=express();
+app.use((req,res, next)=>{
+    console.log('in the middelware');
+    next(); //esta prop te permite que vaya al proximo middleware,
+    // si no ponemos esa propiedad, se finaliza el req-.
+});
 
-console.log(routes.someText);
+app.use((req,res, next)=>{
+    console.log('another middelware');
+    //.. response 
+    res.send('<h1>Hello from express</h1>');
+});
+const server = http.createServer(app);
 
-const server = http.createServer(routes.handler);
+
 
 server.listen(3000);
