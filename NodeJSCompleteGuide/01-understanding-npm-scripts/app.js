427215f9377f716496ae6bb6b0df31337cce0656
German Bertea <gerbertea@gmail.com>
2020-07-25 12:31:54 +0200

    add body parser for post method

--- NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
+++ NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
@@ -1,17 +1,27 @@
 const http = require('http');
 const express = require('express');
+const parser = require('body-parser');
 
 const app=express();
 
+app.use(parser.urlencoded());
 
 app.use('/',(req,res,next)=>{
     console.log('This always runs');
     next();
-})
-app.use('/addProduct', (req,res, next)=>{
-    console.log('another middelware');
-    res.send('<h1>Hello from Products</h1>');
 });
+
+app.use('/add-Product', (req,res, next)=>{
+    console.log('another middelware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></form>');
+});
+
+app.use('/product',(req,res,next)=>{
+    console.log('This always runs',req.body);    
+    res.redirect('/');
+ 
+})
+
 
 app.use('/', (req,res, next)=>{
     console.log('another middelware');
