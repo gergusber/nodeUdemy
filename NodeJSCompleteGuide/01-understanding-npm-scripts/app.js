977b8898f4faa85be34e5973970204e0e594428e
German Bertea <gerbertea@gmail.com>
2020-07-25 16:21:15 +0200

    use 404 status

--- NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
+++ NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
@@ -3,14 +3,15 @@ const express = require('express');
 const parser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { stat } = require('fs');
 const app = express();
 
 app.use(parser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
+app.use((req, res, next) => {
     console.log('Catch any request that isnt defined in the routes ');
-    res.send('<h1>Route not Defined</h1>');
+    res.status(404).send('<h1>Page Not found</h1>');
 });
 app.listen(3000);
