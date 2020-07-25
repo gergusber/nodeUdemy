f628cdcc3ed12891c2efda8f4a1f4a2d154f0fc0
German Bertea <gerbertea@gmail.com>
2020-07-25 10:06:24 +0200

    adding new project and also update with less 64

--- NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
+++ NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
@@ -2,19 +2,21 @@ const http = require('http');
 const express = require('express');
 
 const app=express();
-app.use((req,res, next)=>{
-    console.log('in the middelware');
-    next(); //esta prop te permite que vaya al proximo middleware,
-    // si no ponemos esa propiedad, se finaliza el req-.
+
+
+app.use('/',(req,res,next)=>{
+    console.log('This always runs');
+    next();
+})
+app.use('/addProduct', (req,res, next)=>{
+    console.log('another middelware');
+    res.send('<h1>Hello from Products</h1>');
 });
 
-app.use((req,res, next)=>{
+app.use('/', (req,res, next)=>{
     console.log('another middelware');
-    //.. response 
     res.send('<h1>Hello from express</h1>');
 });
-const server = http.createServer(app);
 
 
-
-server.listen(3000);
+app.listen(3000);
