d9d780d0f5695b6cdd1ad1683f56414021d27516
German Bertea <gerbertea@gmail.com>
2020-08-03 21:05:55 +0200

    Less 72, use Root Directory with routes

--- NodeJSCompleteGuide/01-understanding-npm-scripts/routes/admin.js
+++ NodeJSCompleteGuide/01-understanding-npm-scripts/routes/admin.js
@@ -3,17 +3,18 @@ const path = require('path');
 const router = express.Router();
 
 
+const rootDir = require('../Util/path');
+
 // /admin/add-product => GET
 router.get('/add-Product', (req,res, next)=>{
     console.log('another middelware');
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 
 // /admin/add-product =>  POST
 router.post('/add-product',(req,res,next)=>{
     console.log('This always runs',req.body);    
-    // res.redi(path.join(__dirname,'../','views','add-product.html'));
     res.redirect('/');
 })
 
--- NodeJSCompleteGuide/01-understanding-npm-scripts/routes/shop.js
+++ NodeJSCompleteGuide/01-understanding-npm-scripts/routes/shop.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     console.log('another middelware');
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    res.sendFile(path.join(__dirname,'..','views','shop.html'));
 });
 
 module.exports = router;
