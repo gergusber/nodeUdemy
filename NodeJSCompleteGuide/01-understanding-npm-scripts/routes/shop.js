4bbb039d65316285b40cc41162e748e2a944e7d7
German Bertea <gerbertea@gmail.com>
2020-08-03 21:06:46 +0200

    Update Root dir on shop js

--- NodeJSCompleteGuide/01-understanding-npm-scripts/routes/shop.js
+++ NodeJSCompleteGuide/01-understanding-npm-scripts/routes/shop.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-
+const rootDir = require('../Util/path');
 router.get('/', (req, res, next) => {
     console.log('another middelware');
-    res.sendFile(path.join(__dirname,'..','views','shop.html'));
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports = router;
