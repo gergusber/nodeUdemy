fb1cbfba2b81cb92d982b0ff8ad7b8dae807b67e
German Bertea <german.bertea@altran.com>
2020-12-14 21:38:41 +0100

    Add product styles less 74

--- NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
+++ NodeJSCompleteGuide/01-understanding-npm-scripts/app.js
@@ -8,6 +8,7 @@ const app = express();
 const path = require('path');
 
 app.use(parser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
@@ -15,4 +16,5 @@ app.use((req, res, next) => {
     console.log('Catch any request that isnt defined in the routes ');
     res.status(404).sendFile(path.join(__dirname,'views','not-found.html')); 
 });
+
 app.listen(80);
