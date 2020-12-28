7693259e3370958c204c448bb5f75affd1efd836
German Bertea <german.bertea@altran.com>
2020-12-28 10:16:25 +0100

    Less78

--- Seccion6/00-starting-setup/routes/shop.js
+++ Seccion6/00-starting-setup/routes/shop.js
@@ -5,8 +5,11 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
+
 
 router.get('/', (req, res, next) => {
+ console.log('Shop.JS',adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
