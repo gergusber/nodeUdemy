20deed8f3f55c6254e9a11d9ac381220bf0e85e1
German Bertea <german.bertea@altran.com>
2020-12-29 19:25:01 +0100

    Work on the project

--- Seccion8/00-starting-setup/app.js
+++ Seccion8/00-starting-setup/app.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController= require('./controllers/error')
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404Page);
+app.use(errorController.get404);
 
 app.listen(process.env.PORT);
--- Seccion8/00-starting-setup/models/product.js
+++ Seccion8/00-starting-setup/models/product.js
@@ -1,34 +1,37 @@
-// const products = [];
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
-    path.dirname(process.mainModule.filename),
-    "data",
-    "products.json"
-  );
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, filecontent) => {
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
-    }
-    else{
-        cb(JSON.parse(filecontent));
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
   });
 };
 
 module.exports = class Product {
-  constructor(tit) {
-    this.title = tit;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
+
   save() {
-    getProductsFromFile((products)=>{
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-    });   
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll(cb) {
