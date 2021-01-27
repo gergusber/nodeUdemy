2e8bc697d00a16209c4fde3ab329a94b4ef189d7
German Bertea <german.bertea@altran.com>
2021-01-27 20:54:08 +0100

    Add findbyPk and FindALL

--- Seccion11 Understanding Sequelize/00-starting-setup/controllers/shop.js
+++ Seccion11 Understanding Sequelize/00-starting-setup/controllers/shop.js
@@ -2,51 +2,63 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/product-list", {
-        prods: rows,
+        prods: products,
         pageTitle: "All Products",
         path: "/products",
       });
     })
-    .catch((err) => {
-      console.log(err);
+    .catch((errors) => {
+      console.log(errors);
     });
-
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
-    });
-  });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+
+  // Product.findAll({
+  //   where: {
+  //     id: prodId,
+  //   },
+  // })
+  //   .then((products) => {
+  //     res.render("shop/product-detail", {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((errors) => {
+  //     console.log(errors);
+  //   });
+  Product.findByPk(prodId).then(
+    products =>{
       res.render("shop/product-detail", {
-        product: product[0],
-        pageTitle: product[0].title,
+        product: products,
+        pageTitle: products.title,
         path: "/products",
       });
-    })
-    .catch((err) => console.log(err));
+    }
+  ).catch(
+    errors =>{
+      console.log(errors);
+    }
+  );
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         pageTitle: "Shop",
         path: "/",
       });
     })
-    .catch((err) => {
-      console.log(err);
+    .catch((errors) => {
+      console.log(errors);
     });
 };
 
