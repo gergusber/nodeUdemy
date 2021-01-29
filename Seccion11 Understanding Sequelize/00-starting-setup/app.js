3089695a4f4fa3204995568a2e2d422f8780c271
German Bertea <german.bertea@altran.com>
2021-01-29 23:38:33 +0100

    Utilize Asociation and methos provided by relation

--- Seccion11 Understanding Sequelize/00-starting-setup/app.js
+++ Seccion11 Understanding Sequelize/00-starting-setup/app.js
@@ -22,6 +22,17 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -32,9 +43,23 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 sequalize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
-    // console.log(result);
+    return User.findByPk(1);
+    // console.log(result)
+  })
+  .then((usr) => {
+    if (!usr) {
+      return User.create({
+        name: "German",
+        email: "dummy@email.com",
+      });
+    }
+    return Promise.resolve(usr);
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3002);
   })
   .catch((err) => {
