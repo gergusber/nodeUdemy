5c3866dc09d705269fbd79900c44f421b6919a54
German Bertea <german.bertea@altran.com>
2021-03-21 13:10:13 +0100

    Convert the update value to the id

--- Seccion12 MongoDb/00-starting-setup/controllers/admin.js
+++ Seccion12 MongoDb/00-starting-setup/controllers/admin.js
@@ -1,6 +1,4 @@
 const Product = require("../models/product");
-const mongodb = require("mongodb");
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -59,7 +57,7 @@ exports.postEditProduct = (req, res, next) => {
     updatedPrice,
     updatedDesc,
     updatedImageUrl,
-    new ObjectId(prodId)
+    prodId
   );
   product
     .save()
