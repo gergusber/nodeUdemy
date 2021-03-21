b8131aa74c9ae0c0f60a29cda4e1d1ba5e9cfab5
German Bertea <german.bertea@altran.com>
2021-03-21 11:03:22 +0100

    Connect Mongodb

--- Seccion12 MongoDb/00-starting-setup/util/database.js
+++ Seccion12 MongoDb/00-starting-setup/util/database.js
@@ -1,8 +1,26 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://gerbertea:2cGqcNg6aLYUBaNG@cluster0.yiltf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("CONECTED TO MONGODB");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://<username>:<password>@cluster0.yiltf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
