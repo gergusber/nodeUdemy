b9fed5ed0cd5b1f766e960c6e83cebda451b5c00
German Bertea <german.bertea@altran.com>
2021-03-22 20:25:42 +0100

    First commit, start with mongoose

---
+++ Seccion13 Mongoose/app.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+const app = express();
+const User = require("./models/user");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("60579df424123c58a3bad616")
+    .then((user) => {
+      req.user = new User(user.user, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3002);
+});
