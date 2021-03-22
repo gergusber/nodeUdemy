c9aa866e4e7ad4a3c89fd32bc528fdd823663a4a
German Bertea <german.bertea@altran.com>
2021-03-22 19:16:14 +0100

    Add Delete on cart Items

--- Seccion12 MongoDb/00-starting-setup/models/user.js
+++ Seccion12 MongoDb/00-starting-setup/models/user.js
@@ -64,7 +64,18 @@ class User {
         });
       });
   }
-
+  deleteItemFromCart(prodId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== prodId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
   static findById(userId) {
     const db = getDb();
     return db
