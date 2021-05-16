866f789f9dee8b8742826e063ab06b2cd2e2b782
German Bertea <german.bertea@altran.com>
2021-05-16 18:50:00 +0200

    less 256

--- Seccion15 Auth/controllers/auth.js
+++ Seccion15 Auth/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
@@ -29,7 +30,35 @@ exports.postLogin = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then((usrDoc) => {
+      if (usrDoc) {
+        return res.redirect("/signup");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPass) => {
+          const user = new User({
+            email: email,
+            password: hashedPass,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // User.add
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
