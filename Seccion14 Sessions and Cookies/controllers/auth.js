d4ff61bd18d57bb458c1e5b53a58b5d417c213a5
German Bertea <german.bertea@altran.com>
2021-05-16 16:48:17 +0200

    upate project on session 15

--- Seccion14 Sessions and Cookies/controllers/auth.js
+++ Seccion14 Sessions and Cookies/controllers/auth.js
@@ -1,17 +1,29 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
-  console.log(req.get("Cookie").split("=")[1]);
-  const isLogedIn = req.get("Cookie").split("=")[1];
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLogedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLogedIn = true;
-  res.redirect("/");
+  User.findById("605a3b3b28d44d3c3c473f6a")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
-  // res.setHeader("Set-Cookie", "loggedIn=true");
-  // res.redirect("/");
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
