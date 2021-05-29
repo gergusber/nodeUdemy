5d7b41d541200a5f8ddf0e26c74181ad352266f5
German Bertea <german.bertea@altran.com>
2021-05-29 18:59:42 +0200

    Remove isAuth

--- Seccion15 Auth/controllers/auth.js
+++ Seccion15 Auth/controllers/auth.js
@@ -5,7 +5,6 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
   });
 };
 
@@ -13,7 +12,6 @@ exports.getSignup = (req, res, next) => {
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
   });
 };
 
