f41cb62af22d636837846d9c641aec2481be4d8e
German Bertea <german.bertea@altran.com>
2021-06-13 20:09:04 +0200

    Finish the token reseet

--- Seccion15 Auth/controllers/auth.js
+++ Seccion15 Auth/controllers/auth.js
@@ -198,7 +198,37 @@ exports.getNewPassword = (req, res, next) => {
         pageTitle: "New Pasword",
         errorMessage: mesage,
         userId: usr._id.toString(),
+        passwordToken: token,
       });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+
+  let resetUser;
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((usr) => {
+      resetUser = usr;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPass) => {
+      resetUser.password = hashedPass;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
     })
     .catch((err) => {
       console.log(err);
