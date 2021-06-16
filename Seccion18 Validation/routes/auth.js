bea06cead3115878ca0cd16496eff95348362810
German Bertea <german.bertea@altran.com>
2021-06-16 22:02:46 +0200

    Task from less 297

--- Seccion18 Validation/routes/auth.js
+++ Seccion18 Validation/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { check } = require("express-validator/check");
-
+const { check, body } = require("express-validator/check");
+const User = require("../models/user");
 const authController = require("../controllers/auth");
+const { Error } = require("mongoose");
 
 const router = express.Router();
 
@@ -9,20 +10,58 @@ router.get("/login", authController.getLogin);
 
 router.get("/signup", authController.getSignup);
 
-router.post("/login", authController.postLogin);
+router.post(
+  "/login",
+  [
+    check("email").isEmail().withMessage("Please enter a valid Email."),
+    body(
+      "password",
+      "Please enter a password with only number and text and at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    //   .custom((value, { req }) => {
+    //     return User.findOne({ email: value }).then((user) => {
+    //       if (!user) {
+    //         return Promise.reject("Invalid email or password.");
+    //       }
+    //     });
+    //   }),
+  ],
+  authController.postLogin
+);
 
 router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Please enter a valid Email.")
-    .custom((value, { req }) => {
-      // Add custom Validator
-      if (value == "test@test.com") {
-        throw new Error("This email address is Forbidden.");
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid Email.")
+      .custom((value, { req }) => {
+        // Add custom Validator
+        //  if (value == "test@test.com") {
+        //     throw new Error("This email address is Forbidden.");
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "E-Mail exists already, please pick a different one."
+            );
+          }
+        });
+      }),
+    body(
+      "password",
+      "Please enter a password with only number and text and at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords must to match.");
       }
       return true;
     }),
+  ],
   authController.postSignup
 );
 
