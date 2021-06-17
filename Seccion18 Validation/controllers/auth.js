7e517bd33454a7351d2d0075d8c721bb313a313e
German Bertea <german.bertea@altran.com>
2021-06-17 17:51:59 +0200

    add auth view errors

--- Seccion18 Validation/controllers/auth.js
+++ Seccion18 Validation/controllers/auth.js
@@ -41,6 +41,12 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationErrors: [],
   });
 };
 
@@ -97,6 +103,12 @@ exports.postSignup = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword,
+      },
+      validationErrors: errors.array(),
     });
   }
   bcrypt
