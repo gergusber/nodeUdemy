137aeb548b0c0902e481a94e74e348183deb318f
German Bertea <german.bertea@altran.com>
2021-06-27 16:34:10 +0200

    Add fix for appjs

--- Seccion21 Pagination/app.js
+++ Seccion21 Pagination/app.js
@@ -27,7 +27,10 @@ const fileStorage = multer.diskStorage({
     cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
   },
 });
 
