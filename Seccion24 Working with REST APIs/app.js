c11dd2964e0c0c175e1e8c76c67c67edfd273b0b
German Bertea <german.bertea@altran.com>
2021-06-29 16:54:41 +0200

    init server less364  secc24

---
+++ Seccion24 Working with REST APIs/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const feedRoutes = require("./routes/feed");
+const app = express();
+
+app.use("/feed", feedRoutes);
+
+app.listen(8080);
---
+++ Seccion24 Working with REST APIs/routes/feed.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const express = require("express");
+const feedController = require("../controllers/feed");
+
+const router = express.Router();
+
+//GET: /feed/posts
+router.get("/posts", feedController.getPosts);
+
+module.exports = router;
