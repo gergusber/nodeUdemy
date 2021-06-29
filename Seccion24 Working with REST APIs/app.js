e6840497203d4d0532ee2319af7567f19edf680d
German Bertea <german.bertea@altran.com>
2021-06-29 19:24:10 +0200

    Add post route and get route using express.json

--- Seccion24 Working with REST APIs/app.js
+++ Seccion24 Working with REST APIs/app.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const feedRoutes = require("./routes/feed");
+
 const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+); // application/json
 
 app.use("/feed", feedRoutes);
 
--- Seccion24 Working with REST APIs/routes/feed.js
+++ Seccion24 Working with REST APIs/routes/feed.js
@@ -1,10 +1,13 @@
-const { Router } = require("express");
-const express = require("express");
-const feedController = require("../controllers/feed");
+const express = require('express');
+
+const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
-//GET: /feed/posts
-router.get("/posts", feedController.getPosts);
+// GET /feed/posts
+router.get('/posts', feedController.getPosts);
+
+// POST /feed/post
+router.post('/post', feedController.createPost);
 
 module.exports = router;
