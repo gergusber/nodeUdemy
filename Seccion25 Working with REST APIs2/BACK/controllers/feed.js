a71fc049df5743d82d7566bae35ded8c9237bb59
German Bertea <german.bertea@altran.com>
2021-07-01 21:10:49 +0200

    Add validation to the backend

--- Seccion25 Working with REST APIs2/BACK/controllers/feed.js
+++ Seccion25 Working with REST APIs2/BACK/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -18,7 +20,14 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "validation Failed, enter data is incorrect",
+      errors: errors.array(),
+    });
+  }
   res.status(201).json({
     message: "Post created successfully!",
     post: {
