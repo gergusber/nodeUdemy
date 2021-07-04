0892d9a0c0e9f4375f849be3f04f0ba60fd23d19
German Bertea <german.bertea@altran.com>
2021-07-04 21:52:22 +0200

    Add Get images from public folder

--- Seccion25 Working with REST APIs2/BACK/app.js
+++ Seccion25 Working with REST APIs2/BACK/app.js
@@ -3,8 +3,34 @@ const feedRoutes = require("./routes/feed");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(multer({ storage: storage, fileFilter: fileFilter }).single("image"));
 
 //Static image
 app.use("/images", express.static(path.join(__dirname, "images")));
--- Seccion25 Working with REST APIs2/BACK/controllers/feed.js
+++ Seccion25 Working with REST APIs2/BACK/controllers/feed.js
@@ -25,12 +25,19 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
+  if (!req.file) {
+    const error = new Error("No image provided");
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\", "/");
+
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/38a4b7f301d8ad3435be8bb3d23e4c11.jpg",
+    imageUrl: imageUrl,
     creator: {
       name: "German",
     },
