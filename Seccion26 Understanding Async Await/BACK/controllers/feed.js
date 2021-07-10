fe73dcbe896361f59007a638f52128ed5a9840d0
German Bertea <german.bertea@altran.com>
2021-07-10 12:35:40 +0200

    Add the await and catch errors

--- Seccion26 Understanding Async Await/BACK/controllers/feed.js
+++ Seccion26 Understanding Async Await/BACK/controllers/feed.js
@@ -3,32 +3,28 @@ const Post = require("../models/post");
 const User = require("../models/user");
 const fs = require("fs");
 const path = require("path");
-exports.getPosts = (req, res, next) => {
+
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
-  let totalItems;
 
-  Post.find()
-    .countDocuments()
-    .then((res) => {
-      totalItems = res;
-      return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then((result) => {
-      res.status(200).json({
-        message: "Fetched posts succesfully",
-        posts: result,
-        totalItems: totalItems,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const totalItems = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+
+    res.status(200).json({
+      message: "Fetched posts succesfully",
+      posts: posts,
+      totalItems: totalItems,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.createPost = (req, res, next) => {
