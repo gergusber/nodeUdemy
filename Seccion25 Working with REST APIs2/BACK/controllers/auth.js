6f494bd3fa681aa305d1f75eae44884fa8033cb2
German Bertea <german.bertea@altran.com>
2021-07-10 11:16:42 +0200

    Addd token and login page

--- Seccion25 Working with REST APIs2/BACK/controllers/auth.js
+++ Seccion25 Working with REST APIs2/BACK/controllers/auth.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -26,6 +28,51 @@ exports.signup = (req, res, next) => {
       res.status(201).json({
         message: "User created",
         userId: res._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User with this email couldnt be found");
+        error.statusCode = 401;
+        throw error;
+      }
+
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong password");
+        error.statusCode = 401;
+        throw error;
+      }
+      //we create the jwt
+
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        "secret",
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).json({
+        token: token,
+        userId: loadedUser._id.toString(),
       });
     })
     .catch((err) => {
--- Seccion25 Working with REST APIs2/BACK/routes/auth.js
+++ Seccion25 Working with REST APIs2/BACK/routes/auth.js
@@ -23,4 +23,6 @@ router.put(
   authController.signup
 );
 
+router.post("/login", authController.login);
+
 module.exports = router;
