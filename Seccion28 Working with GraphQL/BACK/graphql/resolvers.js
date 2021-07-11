76bb1c7dfa40ece412ea0ec45be8a308552959ce
German Bertea <german.bertea@altran.com>
2021-07-11 18:36:05 +0200

    Add validations on inputs for mutations

--- Seccion28 Working with GraphQL/BACK/graphql/resolvers.js
+++ Seccion28 Working with GraphQL/BACK/graphql/resolvers.js
@@ -1,9 +1,24 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
-
+const validator = require("validator");
 module.exports = {
   createUser: async function ({ userInput }, req) {
     // const email = args.userInput.email;
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "Email is invalid" });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "Password too short" });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error("Invalid input");
+      throw error;
+    }
 
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
