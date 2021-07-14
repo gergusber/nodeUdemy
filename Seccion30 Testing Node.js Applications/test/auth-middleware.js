4d41af592b376d997eb6aeb69c0eeed4e6235acb
German Bertea <german.bertea@altran.com>
2021-07-14 21:41:22 +0200

    Add test and mock obj

--- Seccion30 Testing Node.js Applications/test/auth-middleware.js
+++ Seccion30 Testing Node.js Applications/test/auth-middleware.js
@@ -1,6 +1,8 @@
 const authMiddleware = require("../middleware/is-auth");
 const chai = require("chai");
 var expect = chai.expect;
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
 
 describe("Auth middleware", function () {
   it("Should throw an error if no authorization headder is present", function () {
@@ -24,5 +26,32 @@ describe("Auth middleware", function () {
       },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("Should throw an error if the token cannot be verified", function () {
+    const req = {
+      get: function () {
+        return "Bearer xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("Should yield a userId after decoding the token", function () {
+    const req = {
+      get: function () {
+        return "Bearer asdasdasd";
+      },
+    };
+
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "abc" });
+    authMiddleware(req, {}, () => {});
+    // expect(req).not.to.throw("Not authenticated ");
+    expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
+
+    jwt.verify.restore();
   });
 });
