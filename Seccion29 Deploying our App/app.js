edde2a668e25a5a88a626afc68531e7fe5e36d5b
German Bertea <german.bertea@altran.com>
2021-07-14 17:26:26 +0200

    Add helmet for https

--- Seccion29 Deploying our App/app.js
+++ Seccion29 Deploying our App/app.js
@@ -8,7 +8,7 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
 const multer = require("multer");
-
+const helmet = require("helmet");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
@@ -48,10 +48,10 @@ const fileFilter = (req, file, cb) => {
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
