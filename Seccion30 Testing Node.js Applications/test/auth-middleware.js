66c09982908efd036d501eaa9c1170627411e17d
German Bertea <german.bertea@altran.com>
2021-07-14 21:14:35 +0200

    Describe wich test we are creating

--- Seccion30 Testing Node.js Applications/test/auth-middleware.js
+++ Seccion30 Testing Node.js Applications/test/auth-middleware.js
@@ -2,16 +2,27 @@ const authMiddleware = require("../middleware/is-auth");
 const chai = require("chai");
 var expect = chai.expect;
 
-it("Should throw an error if no authorization headder is present", function () {
-  const req = {
-    get: function () {
-      return null;
-    },
-  };
-  const error = new Error("Not authenticated");
-  error.statusCode = 401;
+describe("Auth middleware", function () {
+  it("Should throw an error if no authorization headder is present", function () {
+    const req = {
+      get: function () {
+        return null;
+      },
+    };
+    const error = new Error("Not authenticated");
+    error.statusCode = 401;
 
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    "Not authenticated"
-  );
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated"
+    );
+  });
+
+  it("Should throw an error if the authorization headdder is only one string", function () {
+    const req = {
+      get: function () {
+        return "xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
