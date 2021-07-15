f93cba9daf0a165efcb6f19fc06b31a3c0c940cd
German Bertea <german.bertea@altran.com>
2021-07-15 19:21:30 +0200

    Modern way to retrive dirname and filename

--- Seccion32 Modern JavaScript & NodeJS/modern-js-01-starting-setup/modern-js-01-starting-setup/response-handler.js
+++ Seccion32 Modern JavaScript & NodeJS/modern-js-01-starting-setup/modern-js-01-starting-setup/response-handler.js
@@ -1,9 +1,16 @@
-import fs from "fs";
+import path, { dirname } from "path";
+// import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
-  fs.readFile("my-page.html", "utf8", (err, data) => {
-    res.send(data);
-  });
+  //   fs.readFile("my-page.html", "utf8", (err, data) => {
+  //     res.send(data);
+  //   });
+
+  res.sendFile(path.join(__dirname, "my-page.html"));
 };
 
 // export default resHandler;
