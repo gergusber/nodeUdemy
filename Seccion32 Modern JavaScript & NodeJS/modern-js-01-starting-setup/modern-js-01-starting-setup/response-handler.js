eb70e9ee9a1eb71cbb514ef54c9d63aeea15850a
German Bertea <german.bertea@altran.com>
2021-07-15 19:27:32 +0200

    Use promises to get data from fs

--- Seccion32 Modern JavaScript & NodeJS/modern-js-01-starting-setup/modern-js-01-starting-setup/response-handler.js
+++ Seccion32 Modern JavaScript & NodeJS/modern-js-01-starting-setup/modern-js-01-starting-setup/response-handler.js
@@ -1,16 +1,21 @@
 import path, { dirname } from "path";
 // import { dirname } from "path";
 import { fileURLToPath } from "url";
+import fs from "fs/promises";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
-  //   fs.readFile("my-page.html", "utf8", (err, data) => {
-  //     res.send(data);
-  //   });
+  fs.readFile("my-page.html", "utf8")
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-  res.sendFile(path.join(__dirname, "my-page.html"));
+  //   res.sendFile(path.join(__dirname, "my-page.html"));
 };
 
 // export default resHandler;
