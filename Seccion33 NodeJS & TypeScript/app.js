8a69251984892331ba5e0dc2139975ef56e021c5
German Bertea <german.bertea@altran.com>
2021-07-15 20:28:18 +0200

    add multiple types

--- Seccion33 NodeJS & TypeScript/app.js
+++ Seccion33 NodeJS & TypeScript/app.js
@@ -3,15 +3,24 @@ var num1Element = document.getElementById("num1");
 var num2Element = document.getElementById("num2");
 var butonElement = document.querySelector("button");
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + " " + num2;
+    }
+    else {
+        return +num1 + +num2;
+    }
 }
 butonElement.addEventListener("click", function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
+    var stringResult = add(+num1, +num2);
     console.log(result);
+    console.log(stringResult);
 });
-// console.log(add(1, 6));
-// console.log(add("1", "6"));
 //COMPILE WITH tsc app.ts
 //tsc --init  => to create new ts config file
+//tsc => compile all files including the typescript file
--- Seccion33 NodeJS & TypeScript/app.ts
+++ Seccion33 NodeJS & TypeScript/app.ts
@@ -2,15 +2,23 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const butonElement = document.querySelector("button")!;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number | string, num2: number | string) {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else if (typeof num1 === "string" && typeof num2 === "string") {
+    return num1 + " " + num2;
+  } else {
+    return +num1 + +num2;
+  }
 }
 
 butonElement.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = add(+num1, +num2);
+  const stringResult = add(+num1, +num2);
   console.log(result);
+  console.log(stringResult);
 });
 
 //COMPILE WITH tsc app.ts
