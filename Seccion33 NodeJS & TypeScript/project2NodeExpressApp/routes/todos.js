0407cd43e3471e13ae85b98efcfbd66fa4638763
German Bertea <german.bertea@altran.com>
2021-07-18 12:15:13 +0200

    Add types for request body and params

--- Seccion33 NodeJS & TypeScript/project2NodeExpressApp/routes/todos.js
+++ Seccion33 NodeJS & TypeScript/project2NodeExpressApp/routes/todos.js
@@ -2,15 +2,45 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = express_1.Router();
-const todos = [];
+let todos = [];
 router.get("/", (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post("/todo", (req, res, next) => {
+    const body = req.body;
     const newTodo = {
-        text: req.body.text,
+        text: body.text,
         id: new Date().toDateString(),
     };
     console.log("ID:" + newTodo.id + " , text:" + newTodo.text);
+    todos.push(newTodo);
+    return res
+        .status(201)
+        .json({ message: "Created todo", todo: newTodo, todos: todos });
+});
+router.put("/todo/:todoId", (req, res, next) => {
+    const params = req.params;
+    const todoId = params.todoId;
+    const body = req.body;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === todoId);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = {
+            id: todoId,
+            text: body.text,
+        };
+        return res.status(200).json({ message: "Updated todo", todos: todos });
+    }
+    res.status(404).json({ message: "could not find for this id" });
+    const newTodo = {
+        text: body.text,
+        id: new Date().toDateString(),
+    };
+    console.log("ID:" + newTodo.id + " , text:" + newTodo.text);
+    todos.push(newTodo);
+});
+router.delete("/todo/:todoId", (req, res, next) => {
+    const params = req.params;
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
+    res.status(200).json({ message: "Todo item removed properly", todos: todos });
 });
 exports.default = router;
--- Seccion33 NodeJS & TypeScript/project2NodeExpressApp/routes/todos.ts
+++ Seccion33 NodeJS & TypeScript/project2NodeExpressApp/routes/todos.ts
@@ -5,14 +5,16 @@ import { Todo } from "../models/todo";
 const router = Router();
 
 let todos: Todo[] = [];
-
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
 router.get("/", (req, res, next) => {
   res.status(200).json({ todos: todos });
 });
 
 router.post("/todo", (req, res, next) => {
+  const body = req.body as RequestBody;
   const newTodo: Todo = {
-    text: req.body.text,
+    text: body.text,
     id: new Date().toDateString(),
   };
   console.log("ID:" + newTodo.id + " , text:" + newTodo.text);
@@ -23,13 +25,15 @@ router.post("/todo", (req, res, next) => {
     .json({ message: "Created todo", todo: newTodo, todos: todos });
 });
 
-router.put("/todo/:id", (req, res, next) => {
-  const todoId = req.params.id;
+router.put("/todo/:todoId", (req, res, next) => {
+  const params = req.params as RequestParams;
+  const todoId = params.todoId;
+  const body = req.body as RequestBody;
   const todoIndex = todos.findIndex((todoItem) => todoItem.id === todoId);
   if (todoIndex >= 0) {
     todos[todoIndex] = {
       id: todoId,
-      text: req.body.text,
+      text: body.text,
     };
 
     return res.status(200).json({ message: "Updated todo", todos: todos });
@@ -37,7 +41,7 @@ router.put("/todo/:id", (req, res, next) => {
   res.status(404).json({ message: "could not find for this id" });
 
   const newTodo: Todo = {
-    text: req.body.text,
+    text: body.text,
     id: new Date().toDateString(),
   };
   console.log("ID:" + newTodo.id + " , text:" + newTodo.text);
@@ -45,7 +49,8 @@ router.put("/todo/:id", (req, res, next) => {
 });
 
 router.delete("/todo/:todoId", (req, res, next) => {
-  todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+  const params = req.params as RequestParams;
+  todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
   res.status(200).json({ message: "Todo item removed properly", todos: todos });
 });
 export default router;
